449b559bddf7feb842d7307f7ad2eacfe3b9ae80
masanori908 <masanori908@gmail.com>
2026-04-09 03:35:57 +0900

    ビルド設定を簡略化・起動時にmigrateを実行

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,16 +1,17 @@
 """
 WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+# 起動時にマイグレーションを自動実行（Render対応）
+from django.core.management import call_command
+try:
+    call_command('migrate', '--noinput')
+except Exception:
+    pass
 
 application = get_wsgi_application()
